5d972a2eb3745b20ae551c71f2dcdad1a2367ac4
Anime Republic <89608198+Anime-Republic@users.noreply.github.com>
2021-09-04 01:30:29 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,12 +58,7 @@ def stats(update, context):
 
 
 def start(update, context):
-    buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
-    reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
-    if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
-        start_string = f'''
+    start_string = f'''
 This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
