80aef7ac9b0ed156162562915193880ffc88eaa0
anime republic <89608198+anime-republic@users.noreply.github.com>
2021-09-10 02:13:58 +0800

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -36,5 +36,6 @@ class _BotCommands:
         self.DeleteCommand = getCommand('DELETE_BOT', 'del')
         self.ShellCommand = getCommand('SHELL_BOT', 'shell')
         self.ExecHelpCommand = getCommand('EXEHELP_BOT', 'exehelp')
+        self.SpeedCommand = getCommand('SpeedCommand', 'speedtest')
 
 BotCommands = _BotCommands()
