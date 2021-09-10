4b2412e703497501ad75e977e83ea544a15bba65
anime republic <89608198+anime-republic@users.noreply.github.com>
2021-09-11 03:17:27 +0800

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -311,7 +311,7 @@ def _mirror(bot, update, isTar=False, extract=False, isZip=False):
             if bot_utils.is_url(reply_text) or bot_utils.is_magnet(reply_text):
                 link = reply_text
 
-    if bot_utils.is_url(link) and not bot_utils.is_magnet(link) and not os.path.exists(link) and isQbit:
+    if bot_utils.is_url(link) and not bot_utils.is_magnet(link) and not os.path.exists(link):
         resp = requests.get(link)
         if resp.status_code == 200:
             file_name = str(time.time()).replace(".", "") + ".torrent"
