7b7436e2697160825711b95233ff126c4ef92b83
abandoneduser <103356566+abandoneduser@users.noreply.github.com>
2022-04-12 14:31:08 +0545

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -320,7 +320,8 @@ def main():
                                   bot_help, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     stats_handler = CommandHandler(BotCommands.StatsCommand,
                                    stats, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-    log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter, run_async=True)
+    log_handler = CommandHandler(BotCommands.LogCommand, 
+                                 log, filters=CustomFilters.owner_filter | CustomFilters.sudo_user | CustomFilters.mod_user, run_async=True)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(ping_handler)
     dispatcher.add_handler(restart_handler)
